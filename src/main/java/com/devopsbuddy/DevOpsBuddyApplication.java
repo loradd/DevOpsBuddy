cd1508e6a04ca06f12602335dda53bfe70667a06
loradd <lorenzo.addazi@mdh.se>
2018-04-04 23:06:57 +0200

    Initial commit for our first Spring boot project

---
+++ src/main/java/com/devopsbuddy/DevOpsBuddyApplication.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsBuddyApplication.class, args);
+	}
+}
